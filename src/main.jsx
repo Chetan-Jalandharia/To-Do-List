982adc524c4e251389421edb8810a333482fd118
chetan <chetann8900@gmail.com>
2024-06-30 18:36:37 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { Provider } from 'react-redux'
 import  store  from './Store/Store.jsx'
 
